b7b9fb5ce852d4d074b83fe14ebd3e2d063b65b3
yoxenl <caisaskog@gmail.com>
2023-12-16 01:39:12 +0100

    added multiplication calc

--- increasebutton/script.js
+++ increasebutton/script.js
@@ -10,3 +10,29 @@ function resetValue()
 {
     document.getElementById('number').value = 0;
 }
+
+function getMultiTable()
+{
+    var base = parseInt(document.getElementById('base').value, 10);
+    var topValue = parseInt(document.getElementById('topValue').value, 10);
+    var multiTable = [];
+    var para = document.getElementById('multiplicationTable');
+    
+    for (let i = 1; i <= topValue ;i++)
+    {
+        res = i / base;
+        if (!(res % 1))
+        {
+            multiTable.push(i);
+        }
+    }
+
+    para.innerHTML = multiTable;
+}
+
+function resetMultiply()
+{
+    document.getElementById('base').value = '';
+    document.getElementById('topValue').value = '';
+    document.getElementById('multiplicationTable').innerHTML = '';
+}
